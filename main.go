4942df1b2751e6b0ec67026a3b1b94e2f1e5c245
0xgwyn <ali.dindamal@gmail.com>
2023-01-30 14:01:58 +0330

    added comparison functionality

--- main.go
+++ main.go
@@ -7,7 +7,10 @@ import (
 	"log"
 	"net/http"
 	"os"
+	"strings"
 )
+
+const discordURL string = "DISCORD WEBHOOK URL"
 
 func main() {
 
@@ -16,32 +19,79 @@ func main() {
 		"bugcrowd":  "https://raw.githubusercontent.com/arkadiyt/bounty-targets-data/main/data/bugcrowd_data.json",
 		"intigriti": "https://raw.githubusercontent.com/arkadiyt/bounty-targets-data/main/data/intigriti_data.json",
 	}
+
 	for platformName, url := range platformUrls {
 		body := getReq(url)
+		//either create the file if does not exist or check for changes
 		if fileExists(platformName + ".json") {
-			//here we have to compare two jsons, send diff notif then replace the new struct/json with .json file
-			// content := loadFile(platformName + ".json")
-			// oldData, err := json.Marshal(content)
-			// checkError(err)
+			content := loadFileToString(platformName + ".json")
 
-			// var newData interface{}
-			// switch platformName {
-			// case "bugcrowd":
-			// 	newData = stringToStruct[Bugcrowd](body)
-			// case "hackerone":
-			// 	newData = stringToStruct[Hackerone](body)
-			// case "intigriti":
-			// 	newData = stringToStruct[Intigriti](body)
-			// }
+			//compare the most recent changes with the old one
+			changes := compareTargets(content, body, platformName)
+
+			//send changes to the discord server
+			sendNotif(discordURL, changes)
+
+			//replace newest data with the old one
+			saveStringToFile(platformName+".json", body)
 
 		} else {
 			saveStringToFile(platformName+".json", body)
 		}
+
 	}
 }
 
-func stringToStruct[platforms Bugcrowd | Hackerone | Intigriti](data string) platforms {
-	var p platforms
+func compareTargets(old, new, platform string) map[string][]string {
+	changes := make(map[string][]string)
+	//remove junk characters for comparison
+	old = strings.Replace(strings.Replace(strings.Replace(old, "\\t", "", -1), "\\\"", "", -1), "", "", -1)
+
+	switch platform {
+	case "bugcrowd":
+		newData := stringToStruct[Bugcrowd](new)
+		for _, company := range newData {
+			for _, inscope := range company.Targets.InScope {
+				//remove junk characters for comparison
+				target := strings.Replace(strings.Replace(strings.Replace(inscope.Target, "\t", "", -1), "\"", "", -1), "", "", -1)
+				if !strings.Contains(old, target) {
+					changes[company.Name] = append(changes[company.Name], target)
+				}
+			}
+		}
+	case "hackerone":
+		newData := stringToStruct[Hackerone](new)
+		for _, company := range newData {
+			for _, inscope := range company.Targets.InScope {
+				//remove junk characters for comparison
+				target := strings.Replace(strings.Replace(strings.Replace(inscope.AssetIdentifier, "\t", "", -1), "\"", "", -1), "", "", -1)
+				if !strings.Contains(old, target) {
+					changes[company.Name] = append(changes[company.Name], target)
+				}
+			}
+		}
+	case "intigriti":
+		newData := stringToStruct[Intigriti](new)
+		for _, company := range newData {
+			for _, inscope := range company.Targets.InScope {
+				//remove junk characters for comparison
+				target := strings.Replace(strings.Replace(strings.Replace(inscope.Endpoint, "\t", "", -1), "\"", "", -1), "", "", -1)
+				if !strings.Contains(old, target) {
+					changes[company.Name] = append(changes[company.Name], target)
+				}
+			}
+		}
+	}
+
+	return changes
+}
+
+type platformtype interface {
+	Bugcrowd | Hackerone | Intigriti
+}
+
+func stringToStruct[platform platformtype](data string) []platform {
+	var p []platform
 	err := json.Unmarshal([]byte(data), &p)
 	checkError(err)
 	return p
@@ -72,13 +122,18 @@ func getReq(url string) string {
 
 func saveStringToFile(path string, content string) {
 	file, err := os.OpenFile(path, os.O_WRONLY|os.O_CREATE, 0644)
+	// err := ioutil.WriteFile(path, []byte(content), 0644)
 	checkError(err)
 	defer file.Close()
 	fmt.Fprint(file, content)
 }
 
-func loadFile(path string) string {
+func loadFileToString(path string) string {
 	content, err := ioutil.ReadFile(path)
 	checkError(err)
 	return string(content)
+}
+
+func sendNotif(url string, changes map[string][]string) {
+	// to be implemented
 }
