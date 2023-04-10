d86479ac9e505c7f38a9c95202ff36d1e3f5d73a
0xgwyn <ali.dindamal@gmail.com>
2023-04-10 15:10:47 +0330

    Only check chaos repo changes

--- main.go
+++ main.go
@@ -8,42 +8,25 @@ import (
 	"log"
 	"net/http"
 	"os"
-	"strings"
 	"time"
 )
 
-const discordURL string = "DISCORD'S WEBHOOK URL"
+const (
+	discordURL string = "DISCORD'S WEBHOOK URL"
+	chaosUrl          = "https://raw.githubusercontent.com/projectdiscovery/public-bugbounty-programs/main/chaos-bugbounty-list.json"
+)
 
-func main() {
-	platformUrls := map[string]string{
-		"hackerone": "https://raw.githubusercontent.com/arkadiyt/bounty-targets-data/main/data/hackerone_data.json",
-		"bugcrowd":  "https://raw.githubusercontent.com/arkadiyt/bounty-targets-data/main/data/bugcrowd_data.json",
-		"intigriti": "https://raw.githubusercontent.com/arkadiyt/bounty-targets-data/main/data/intigriti_data.json",
-	}
+// ChaosProgram json data item struct
+type ChaosProgram struct {
+	Name    string   `json:"name"`
+	URL     string   `json:"url"`
+	Bounty  bool     `json:"bounty"`
+	Swag    bool     `json:"swag"`
+	Domains []string `json:"domains"`
+}
 
-	for platformName, url := range platformUrls {
-		body := getReq(url)
-
-		//either create the file if does not exist or check for changes
-		if fileExists(platformName + ".json") {
-			content := loadFileToString(platformName + ".json")
-
-			//compare the most recent changes with the old one
-			changes := compareTargets(content, body, platformName)
-
-			//send changes to the discord server
-			sendNotif(discordURL, platformName, changes)
-
-			//replace newest data with the old ones
-			saveStringToFile(platformName+".json", body)
-
-		} else {
-			saveStringToFile(platformName+".json", body)
-		}
-
-	}
-
-	healthCheck()
+type ChaosList struct {
+	Programs []ChaosProgram `json:"programs"`
 }
 
 type companyChanges struct {
@@ -56,30 +39,36 @@ type webhookPayload struct {
 	Contents string `json:"content"`
 }
 
-func healthCheck(){
+func main() {
 
-	file, err := os.OpenFile("health_check", os.O_WRONLY|os.O_CREATE|os.O_APPEND, 0644)
-	if err != nil {
-		panic(err)
+	//request json data from github
+	resp := getReq(chaosUrl)
+
+	//either create the file if does not exist or check for changes
+	if fileExists("chaos.json") {
+		content := loadFileToString("chaos.json")
+
+		//compare the most recent changes with the old one
+		changes := compareTargets(content, resp)
+
+		//send changes to the discord server
+		sendNotif(changes)
+
+		//replace new data with the old one
+		saveStringToFile("chaos.json", resp)
+
+	} else {
+		saveStringToFile("chaos.json", resp)
 	}
-	defer file.Close()
 
-	time := getCurrentTime()
-	file.WriteString(time+" working fine\n")
+	//log program's activity
+	healthCheck()
 }
 
-func getCurrentTime() string {
-	currentTime := time.Now()
-	formattedTime := fmt.Sprintf("%v/%v/%v %v:%v:%v", currentTime.Year(), int(currentTime.Month()), currentTime.Day(), currentTime.Hour(), currentTime.Minute(), currentTime.Second())
-
-	return formattedTime
-}
-
-
-func sendNotif(url, platform string, changes []companyChanges) {
-	// to be implemented
+func sendNotif(changes []companyChanges) {
+	//beautify it for discord markup
 	for _, company := range changes {
-		content := fmt.Sprintf("**%v** (%v)\n*URL*: <%v>\n*Assets*:\n```", company.name, platform, company.url)
+		content := fmt.Sprintf("**%v**\n*URL*: <%v>\n*Assets*:\n```", company.name, company.url)
 		for _, asset := range company.changes {
 			content += asset + "\n"
 		}
@@ -88,94 +77,49 @@ func sendNotif(url, platform string, changes []companyChanges) {
 		postData, err := json.Marshal(webhookPayload{Contents: content})
 		checkError(err)
 
-		http.Post(url, "application/json", bytes.NewBuffer(postData))
+		http.Post(discordURL, "application/json", bytes.NewBuffer(postData))
 	}
 }
 
-func compareTargets(old, new, platform string) []companyChanges {
+func compareTargets(old, new string) []companyChanges {
 	changes := []companyChanges{}
 
-	//remove junk characters for comparison
-	old = strings.Replace(strings.Replace(strings.Replace(old, "\\t", "", -1), "\\\"", "", -1), "", "", -1)
+	//unmarshal old and new data as ChaosList
+	newData := stringToStruct(new)
+	oldData := stringToStruct(old)
 
-	switch platform {
+	//use all old domains as map index
+	allOldDomains := make(map[string]bool)
+	for _, oldProgram := range oldData.Programs {
+		for _, oldDomain := range oldProgram.Domains {
+			allOldDomains[oldDomain] = true
+		}
+	}
 
-	case "bugcrowd":
-		newData := stringToStruct[Bugcrowd](new)
+	for _, newProgram := range newData.Programs {
+		var assetChanges []string
 
-		for _, company := range newData {
-			var assetChanges []string
-
-			for _, inscope := range company.Targets.InScope {
-				//remove junk characters for comparison
-				target := strings.Replace(strings.Replace(strings.Replace(inscope.Target, "\t", "", -1), "\"", "", -1), "", "", -1)
-
-				if !strings.Contains(old, target) {
-					assetChanges = append(assetChanges, target)
-				}
-			}
-
-			// add changes if a company has new assets
-			if len(assetChanges) != 0 {
-				changes = append(changes, companyChanges{company.Name, company.URL, assetChanges})
+		//check if each domain is present in the old database
+		for _, newDomain := range newProgram.Domains {
+			if _, exist := allOldDomains[newDomain]; !exist {
+				assetChanges = append(assetChanges, newDomain)
 			}
 		}
 
-	case "hackerone":
-		newData := stringToStruct[Hackerone](new)
-
-		for _, company := range newData {
-			var assetChanges []string
-
-			for _, inscope := range company.Targets.InScope {
-				//remove junk characters for comparison
-				target := strings.Replace(strings.Replace(strings.Replace(inscope.AssetIdentifier, "\t", "", -1), "\"", "", -1), "", "", -1)
-
-				if !strings.Contains(old, target) {
-					assetChanges = append(assetChanges, target)
-				}
-			}
-
-			// add changes if a company has new assets
-			if len(assetChanges) != 0 {
-				changes = append(changes, companyChanges{company.Name, company.URL, assetChanges})
-			}
-		}
-
-	case "intigriti":
-		newData := stringToStruct[Intigriti](new)
-
-		for _, company := range newData {
-			var assetChanges []string
-
-			for _, inscope := range company.Targets.InScope {
-				//remove junk characters for comparison
-				target := strings.Replace(strings.Replace(strings.Replace(inscope.Endpoint, "\t", "", -1), "\"", "", -1), "", "", -1)
-
-				if !strings.Contains(old, target) {
-					assetChanges = append(assetChanges, target)
-				}
-			}
-
-			// add changes if a company has new assets
-			if len(assetChanges) != 0 {
-				changes = append(changes, companyChanges{company.Name, company.URL, assetChanges})
-			}
+		// add changes if a company has new assets
+		if len(assetChanges) != 0 {
+			changes = append(changes, companyChanges{newProgram.Name, newProgram.URL, assetChanges})
 		}
 	}
 
 	return changes
 }
 
-type platformtype interface {
-	Bugcrowd | Hackerone | Intigriti
-}
-
-func stringToStruct[platform platformtype](data string) []platform {
-	var p []platform
-	err := json.Unmarshal([]byte(data), &p)
+func stringToStruct(data string) ChaosList {
+	var s ChaosList
+	err := json.Unmarshal([]byte(data), &s)
 	checkError(err)
-	return p
+	return s
 }
 
 func fileExists(path string) bool {
@@ -213,4 +157,23 @@ func loadFileToString(path string) string {
 	content, err := ioutil.ReadFile(path)
 	checkError(err)
 	return string(content)
+}
+
+func healthCheck() {
+
+	file, err := os.OpenFile("health_check", os.O_WRONLY|os.O_CREATE|os.O_APPEND, 0644)
+	if err != nil {
+		panic(err)
+	}
+	defer file.Close()
+
+	time := getCurrentTime()
+	file.WriteString(time + " working fine\n")
+}
+
+func getCurrentTime() string {
+	currentTime := time.Now()
+	formattedTime := fmt.Sprintf("%v/%v/%v %v:%v:%v", currentTime.Year(), int(currentTime.Month()), currentTime.Day(), currentTime.Hour(), currentTime.Minute(), currentTime.Second())
+
+	return formattedTime
 }
